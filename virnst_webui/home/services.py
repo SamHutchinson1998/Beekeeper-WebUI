5e2b5254ef6cbdc8d3ae0388d5159d368482b756
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-11 03:32:58 +0000

    fixed 'Could not parse remainder' error

--- virnst_webui/home/services.py
+++ virnst_webui/home/services.py
@@ -1,7 +1,10 @@
 import libvirt
 
 def get_domains():
+  domain_list = []
   conn = libvirt.open('qemu:///system')
   domains = conn.listAllDomains(0)
+  for domain in domains:
+    domain_list.append(domain)
   conn.close()
-  return domains
+  return domain_list
