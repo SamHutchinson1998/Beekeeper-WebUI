5c9535908fcef43677d739d764e394bd1b3a3b0b
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-11 20:17:37 +0000

    stylesheets for draggable objects are now referenced in the base file in the head.

--- virnst_webui/assets/javascript/draggables.js
+++ virnst_webui/assets/javascript/draggables.js
@@ -1,53 +1,32 @@
-import interact from 'interactjs'
+// target elements with the "draggable" class
+interact('.draggable')  
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+  });
 
-const position = { x: 0, y: 0 }
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-interact('.domain').draggable({
-  onmove: function(event) {
-    const target = event.target;
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
 
-    const dataX = target.getAttribute('data-x');
-    const dataY = target.getAttribute('data-y');
-    const initialX = parseFloat(dataX) || 0;
-    const initialY = parseFloat(dataY) || 0;
-
-    const deltaX = event.dx;
-    const deltaY = event.dy;
-
-    const newX = initialX + deltaX;
-    const newY = initialY + deltaY;
-
-    target
-      .style
-      .transform = `translate(${newX}px, ${newY}px)`;
-
-    target.setAttribute('data-x', newX);
-    target.setAttribute('data-y', newY);
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
   }
-})
-
-function onMove (event) {
-  const event = target.event;
-
-  const dataX = target.getAttribute('data-x');
-  const dataY = target.getAttribute('data-y');
-  const initialX = parseFloat(dataX) || 0;
-  const initialY = parseFloat(dataY) || 0;
-
-  const deltaX = event.dx;
-  const deltaY = event.dy;
-
-  const newX = initialX + deltaX;
-  const newY = initialY + deltaY;
-
-  target
-    .style
-    .transform = `translate(${newX}px, ${newY}px)`;
-
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-}
-
-items.draggable({
-  onmove: onMove(event)
-})
