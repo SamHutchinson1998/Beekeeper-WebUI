ae262e963f276aad7f90a3dfe29f314aa38d008b
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-11 05:14:21 +0000

    Home page now displays port info for a domain

--- virnst_webui/home/services.py
+++ virnst_webui/home/services.py
@@ -6,5 +6,16 @@ def get_domains():
   domains = conn.listAllDomains(0)
   for domain in domains:
     domain_list.append(domain.name())
+    port = get_domain_port(domain)
+    domain_list.append(port)
   conn.close()
   return domain_list
+
+def get_domain_port(domain):
+  port = 5900 #first port a domain attaches to if there are no other domains
+  raw_xml = domain.XMLDesc(0)
+  xml = minidom.parseString(raw_xml)
+  graphicsTypes = cml.getElementsBytagName('graphics')
+  for graphicsType in graphicsTypes:
+    port = graphicsType.getAttribute('port')
+  return port
