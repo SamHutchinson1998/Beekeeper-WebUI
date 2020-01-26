d37f216f3af2378d274d1e40063ae490009b8ab9
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-26 14:44:31 +0000

    Image form should now include bootstrap

--- virnst_webui/home/models.py
+++ virnst_webui/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import ModelForm
+from django import forms
 
 # Create your models here.
 
@@ -16,7 +16,24 @@ class DiskImage(models.Model):
   disk_image = models.FileField(upload_to='disk_images/')
   #image = models.ImageField()
 
-class ImageForm(ModelForm):
+class ImageForm(forms.ModelForm):
   class Meta:
     model = DiskImage
     fields = ['name', 'devicetype', 'disk_image']
+    widgets = {
+      'name': forms.TextInput(
+        attrs={
+          'class': 'form-control'
+        }
+      )
+      'devicetype': forms.CharField(
+        attrs={
+          'class': 'form-control'
+        }
+      )
+      'disk_image': forms.FileField(
+        attrs={
+          'class': 'form-control'
+        }
+      )
+    }
