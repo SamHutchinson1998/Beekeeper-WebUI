c96ff55130cc64c840fda7c4fb0bc109ea52ddf8
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-28 17:47:06 +0000

    javascript moved into _sidebar.html

--- virnst_webui/assets/javascript/sidebar.js
+++
@@ -1,7 +0,0 @@
-const list = document.getElementById('list')
-
-for (let i = 0; i < 50; i++) {
-  const listItem = document.createElement('li')
-  listItem.innerHTML = `list-item ${i}`
-  list.appendChild(listItem)
-}
