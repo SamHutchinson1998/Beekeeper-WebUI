f459a8b3882f742c795c1567df4c424712523358
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-01-30 23:24:28 +0000

    Fixed a bug in urls.py

--- virnst_webui/home/urls.py
+++ virnst_webui/home/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('home', HomePageView.saveXml, name='save_xml'),
     path('retrieveXml', HomePageView.retrieveXml, name='retrieve_xml'),
-    path('upload_image', HomePageView.upload_images, name='upload_image')
+    path('upload_image', HomePageView.upload_images, name='upload_image'),
     path('get_devices',HomePageView.get_devices, name='get_devices')
 ]
