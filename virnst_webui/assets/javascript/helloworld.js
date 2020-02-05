33efd0891dc1789e70d713932b9321f59b9d3b9c
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-05 15:40:32 +0000

    graph removes newly-created object from the graph when the modal goes away

--- virnst_webui/assets/javascript/helloworld.js
+++ virnst_webui/assets/javascript/helloworld.js
@@ -84,6 +84,12 @@ function addSidebarIcon(sidebar, graph, disk_image, image_id)
   var funct = function(graph, evt, cell, x, y)
   {
     $('#device_modal').modal('show');
+    $('#device_modal').on("hidden.bs.modal", function () {
+      if (graph.isEnabled())
+      {
+        graph.removeCells();
+      }
+    });
     //getDeviceModal(image_id)
     // This function can take the image ID too!!
     var parent = graph.getDefaultParent();
