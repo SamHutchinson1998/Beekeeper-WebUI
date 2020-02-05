ef69b2ac31bfbabcf58da599685b445f5327ef9f
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-05 16:03:14 +0000

    Further specified removal of a device only when a close button is clicked in the add device modal

--- virnst_webui/assets/javascript/helloworld.js
+++ virnst_webui/assets/javascript/helloworld.js
@@ -84,12 +84,12 @@ function addSidebarIcon(sidebar, graph, disk_image, image_id)
   var funct = function(graph, evt, cell, x, y)
   {
     $('#device_modal').modal('show');
-    $('#device_modal').on("hidden.bs.modal", function () {
+    document.getElementById('close_button').onclick = function removeDevice(){
       if (graph.isEnabled())
       {
         graph.removeCells();
       }
-    });
+    }
     //getDeviceModal(image_id)
     // This function can take the image ID too!!
     var parent = graph.getDefaultParent();
