99f9897243dcf0d5bf374cc23669b877e1536fc5
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-08 13:17:37 +0000

    Re-added the name field to the Virtual Machines model

--- virnst_webui/home/models.py
+++ virnst_webui/home/models.py
@@ -33,6 +33,7 @@ class DiskImage(models.Model):
   disk_image = models.FileField(upload_to='disk_images/')
 
 class VirtualMachine(models.Model):
+  name = models.CharField(max_length=100)
   ram = models.CharField(max_length=8)
   disk_size = models.IntegerField()
   cpus = models.IntegerField()
@@ -46,4 +47,4 @@ class ImageForm(forms.ModelForm):
 class VirtualMachineForm(forms.ModelForm):
   class Meta:
     model = VirtualMachine
-    fields = ['ram', 'disk_size', 'cpus', 'disk_image']
+    fields = ['name','ram', 'disk_size', 'cpus', 'disk_image']
