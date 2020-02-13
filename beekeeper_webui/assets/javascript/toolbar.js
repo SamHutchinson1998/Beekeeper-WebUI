1a804dc771acbc05592a403d5075d17118896b6d
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-13 11:25:56 +0000

    toolbar *should* be draggable now.

---
+++ beekeeper_webui/assets/javascript/toolbar.js
@@ -0,0 +1,27 @@
+interact('.toolbarContainer')
+  .draggable({
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    onmove: dragMoveListener,
+    // enable autoScroll
+    autoScroll: true,
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+window.dragMoveListener = dragMoveListener
