a97d52f578cf9e0e3a82c839419fd698ee72cdd1
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-15 13:16:43 +0000

    Toolbar is now at the top and is no longer draggable

--- beekeeper_webui/assets/javascript/toolbar.js
+++ beekeeper_webui/assets/javascript/toolbar.js
@@ -1,23 +1,43 @@
-interact('#toolbarContainer').draggable({
-  onmove: dragMoveListener,
-  modifiers: [
-    interact.modifiers.restrictRect({
-      restriction: 'parent',
-      endOnly: true
-    })
-  ],
-})
+function populateToolbar(graph)
+{
+  toolbar = document.getElementById('toolbarcontainer')
+}
 
-function dragMoveListener (event) {
-  var target = event.target
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+function addToolbarIcon(toolbar, graph, tool)
+{
+  var funct = function(graph, evt, cell, x, y)
+  {
+    var parent = graph.getDefaultParent();
+    var model = graph.getModel();
+    
+    var cable = null;
+    var stylesheet = `shape=image;image=${image};` +
+    `verticalLabelPosition=bottom;verticalAlign=top;`;
+    model.beginUpdate();
+    try
+    {
+      cable = graph.insertVertex(parent, null, '', x, y);
+      cable.setConnectable(true);
+    }
+    finally
+    {
+      model.endUpdate();
+    }
+    graph.setSelectionCell(cable);
+  }
 
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)'
+  var icon = document.createElement('img');
+  icon.setAttribute('src', tool);
+  icon.setAttribute('id', 'sidebarItem');
+  icon.title = 'Drag this onto the canvas to create a new ethernet connection';
+  toolbar.appendChild(icon);
 
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
+  var dragElement = document.createElement('div');
+  dragElement.style.border = 'dashed black 1px';
+  dragElement.style.width = '150px';
+  dragElement.style.height = '150px';
+
+  var ds = mxUtils.makeDraggable(icon,graph,funct,dragElement,0,0,true,true);
+  ds.setGuidesEnabled(true);
 }
 window.dragMoveListener = dragMoveListener
