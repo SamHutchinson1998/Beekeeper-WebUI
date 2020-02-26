697a0ccde1a0ee38991645d080233a66c64482db
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-02-26 14:08:32 +0000

    More changes to vnc.js

--- beekeeper_webui/assets/javascript/vnc.js
+++ beekeeper_webui/assets/javascript/vnc.js
@@ -6,39 +6,6 @@ import RFB from './noVNC/core/rfb.js';
 let rfb;
 let desktopName;
 
-// When this function is called we have
-// successfully connected to a server
-function connectedToServer(e) {
-    status("Connected to " + desktopName);
-}
-
-// This function is called when we are disconnected
-function disconnectedFromServer(e) {
-    if (e.detail.clean) {
-        status("Disconnected");
-    } else {
-        status("Something went wrong, connection is closed");
-    }
-}
-
-// When this function is called, the server requires
-// credentials to authenticate
-function credentialsAreRequired(e) {
-    const password = prompt("Password Required:");
-    rfb.sendCredentials({ password: password });
-}
-
-// When this function is called we have received
-// a desktop name from the server
-function updateDesktopName(e) {
-    desktopName = e.detail.name;
-}
-
-// Show a status text in the top bar
-function status(text) {
-    document.getElementById('status').textContent = text;
-}
-
 // This function extracts the value of one variable from the
 // query string. If the variable isn't defined in the URL
 // it returns the default value instead.
@@ -58,9 +25,6 @@ function readQueryVariable(name, defaultValue) {
 
     return defaultValue;
 }
-
-// Read parameters specified in the URL query string
-// By default, use the host and port of server that served this file
 
 //const host = readQueryVariable('host', window.location.hostname);
 //let port = readQueryVariable('port', window.location.port);
@@ -91,12 +55,6 @@ url += '/' + path;
 // Creating a new RFB object will start a new connection
 rfb = new RFB(document.getElementById('screen'), url,
               { credentials: { password: password } });
-
-// Add listeners to important events from the RFB module
-rfb.addEventListener("connect",  connectedToServer);
-rfb.addEventListener("disconnect", disconnectedFromServer);
-rfb.addEventListener("credentialsrequired", credentialsAreRequired);
-rfb.addEventListener("desktopname", updateDesktopName);
 
 // Set parameters that can be changed on an active connection
 rfb.viewOnly = readQueryVariable('view_only', false);
