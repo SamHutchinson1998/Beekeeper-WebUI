18926f0f8b147823d8c982f32a25851603ea1a77
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-04-21 15:02:28 +0100

    commit

--- beekeeper_webui/home/tests.py
+++ beekeeper_webui/home/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Device, DiskImage
 
-# Create your tests here.
+# Models tests
+
+def create_image(self, name, devicetype, diskimage):
+  return DiskImage.objects.create(name=name, devicetype=devicetype, disk_image=diskimage)
+
+class DiskImageTest(TestCase):
+
+  def test_image_creation(self):
+    image = self.create_image('test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    # copy - test the uniqueness of the name field
+    self.assertTrue(isinstance(image,DiskImage))
+
+class DeviceTest(TestCase):
+
+  def create_device(self, name, ram, disk_size, cpus, cell_id, disk_image, token, console_port):
+    return Device.objects.create(name=name, ram=ram,disk_size=disk_size)
+  
+  def test_device_creation(self):
+    image = self.create_image('test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    device = self.create_device('test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', '10015')
+    self.assertTrue(isinstance(device,Device))
