a0442bfe5ebcf99761cc77c42d3acd69c30d1251
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-04-21 14:15:31 +0000

    commit

--- beekeeper_webui/home/tests.py
+++ beekeeper_webui/home/tests.py
@@ -9,16 +9,16 @@ def create_image(self, name, devicetype, diskimage):
 class DiskImageTest(TestCase):
 
   def test_image_creation(self):
-    image = self.create_image('test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    image = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
     # copy - test the uniqueness of the name field
     self.assertTrue(isinstance(image,DiskImage))
 
 class DeviceTest(TestCase):
 
   def create_device(self, name, ram, disk_size, cpus, cell_id, disk_image, token, console_port):
-    return Device.objects.create(name=name, ram=ram,disk_size=disk_size)
+    return Device.objects.create(name=name, ram=ram, disk_size=disk_size, cpus=cpus, cell_id=cell_id, disk_image=disk_image, token=token, console_port=console_port)
   
   def test_device_creation(self):
-    image = self.create_image('test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
-    device = self.create_device('test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', '10015')
+    image = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    device = self.create_device('test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     self.assertTrue(isinstance(device,Device))
