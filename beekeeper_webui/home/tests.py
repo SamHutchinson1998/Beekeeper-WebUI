4cb61a6afa11c22638bcf446b811cce2d569ad38
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-04-22 12:02:40 +0000

    commit

--- beekeeper_webui/home/tests.py
+++ beekeeper_webui/home/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django.db import IntegrityError
 from .models import Device, DiskImage, EthernetPorts, EthernetCable
 from .models import EthernetPortsForm, ImageForm, DeviceForm
@@ -79,28 +79,10 @@ class EthernetCableTest(TestCase):
 
 # Forms tests
 
-class EthernetPortsForm(TestCase):
-
-  def test_valid_ethernet_port(self):
-    image = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
-    device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
-    ethernet_port = create_ethernet_ports(self, device, 'MA:CA:DD:RE:SS:XD')
-    data = {'virtual_machine': ethernet_port.virtual_machine, 'mac_address': ethernet_port.mac_address}
-    form = EthernetPortsForm(data)
-    self.assertTrue(form.is_valid())
-
-  def test_invalid_ethernet_port(self):
-    image = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
-    device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
-    ethernet_port = create_ethernet_ports(self, device, 'MA:CA:DD:RE:SS:XD'*20) # mac_address is greater than length 48 so should fail
-    data = {'virtual_machine': ethernet_port.virtual_machine, 'mac_address': ethernet_port.mac_address}
-    form = EthernetPortsForm(data)
-    self.assertFalse(form.is_valid())
-
-class ImageFormTest(TestCase):
+class ImageFormTest(TransactionTestCase):
 
   def test_valid_image_form(self):
-    image = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    image = create_image(self, 'test_image_2', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
     data = {'name': image.name, 'devicetype': image.devicetype, 'disk_image': image.disk_image}
     form = ImageForm(data)
     self.assertTrue(form.is_valid())
@@ -110,13 +92,13 @@ class ImageFormTest(TestCase):
     image.save()
     with self.assertRaises(Exception) as raised:
       image2 = create_image(self, 'test_image', 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
+    DiskImage.objects.get(name=image.name).delete()
     self.assertEqual(IntegrityError, type(raised.exception))
 
   def test_image_max_length(self):
     image = create_image(self, 'a'*101, 'pc', '../ubuntu-18.04.2-live-server-amd64.iso')
     data = {'name': image.name, 'devicetype': image.devicetype, 'disk_image': image.disk_image}
     form = ImageForm(data)
-    print(form.errors)
     self.assertFalse(form.is_valid())
 
 class DeviceFormTest(TestCase):
