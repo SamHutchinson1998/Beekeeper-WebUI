5cd669ff9aedacddfa6c2d54cd8dbe8d28857544
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-04-23 20:18:27 +0100

    commit

--- beekeeper_webui/home/tests.py
+++ beekeeper_webui/home/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase, TransactionTestCase
 from django.db import IntegrityError
 from django.urls import reverse
 from django.core.serializers import serialize
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.conf import settings
+from django.core.files.uploadedfile import)rom django.conf import settings
 from .models import Device, DiskImage, EthernetPorts, EthernetCable
 from .models import ImageForm, DeviceForm
 import json
@@ -17,7 +16,9 @@ import os
 
 # Models tests
 
-def create_image(self, name, devicetype, diskimage):
+def create_image(self, name, devicetype):
+  diskimage = open('../ubuntu-18.04.2-live-server-amd64.iso')
+  diskimage.name, diskimage.read())
   return DiskImage.objects.create(name=name, devicetype=devicetype, disk_image=diskimage)
 
 def create_device(self, name, ram, disk_size, cpus, cell_id, disk_image, token, console_port):
@@ -29,21 +30,21 @@ def create_ethernet_ports(self, virtual_machine, mac_address):
 class DiskImageTest(TestCase):
 
   def test_image_creation(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     # copy - test the uniqueness of the name field
     self.assertTrue(isinstance(image,DiskImage))
 
 class DeviceTest(TestCase):
   
   def test_device_creation(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     self.assertTrue(isinstance(device,Device))
 
 class EthernetPortsTest(TestCase):
     
   def test_ethernet_ports_creation(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     ethernet_port = create_ethernet_ports(self, device, 'MA:CA:DD:RE:SS:XD')
     self.assertTrue(isinstance(ethernet_port, EthernetPorts))
@@ -54,7 +55,7 @@ class EthernetCableTest(TestCase):
     return EthernetCable.objects.create(name=name, source=source, target=target, cell_id=cell_id)
 
   def test_ethernet_cable(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     source_device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     source = create_ethernet_ports(self, source_device, 'MA:CA:DD:RE:SS:XD')
     target_device = create_device(self, 'test_device_2', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
@@ -64,7 +65,7 @@ class EthernetCableTest(TestCase):
     self.assertTrue(isinstance(ethernet_cable, EthernetCable))
 
   def test_ethernet_cable_wo_source(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     target_device = create_device(self, 'test_device_2', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     target = create_ethernet_ports(self, target_device, 'MA:CA:DD:RE:SS:XD')
 
@@ -72,7 +73,7 @@ class EthernetCableTest(TestCase):
     self.assertTrue(isinstance(ethernet_cable, EthernetCable))
 
   def test_ethernet_cable_wo_target(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     source_device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     source = create_ethernet_ports(self, source_device, 'MA:CA:DD:RE:SS:XD')
 
@@ -88,21 +89,21 @@ class EthernetCableTest(TestCase):
 class ImageFormTest(TransactionTestCase):
 
   def test_valid_image_form(self):
-    image = create_image(self, 'test_image_2', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image_2', 'pc')
     data = {'name': image.name, 'devicetype': image.devicetype, 'disk_image': image.disk_image}
     form = ImageForm(data)
     self.assertTrue(form.is_valid())
 
   def test_image_unique_name(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     image.save()
     with self.assertRaises(Exception) as raised:
-      image2 = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+      image2 = create_image(self, 'test_image', 'pc')
     DiskImage.objects.get(name=image.name).delete()
     self.assertEqual(IntegrityError, type(raised.exception))
 
   def test_image_max_length(self):
-    image = create_image(self, 'a'*101, 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'a'*101, 'pc')
     data = {'name': image.name, 'devicetype': image.devicetype, 'disk_image': image.disk_image}
     form = ImageForm(data)
     self.assertFalse(form.is_valid())
@@ -110,35 +111,35 @@ class ImageFormTest(TransactionTestCase):
 class DeviceFormTest(TransactionTestCase):
 
   def test_valid_device_form(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     data = {'name': device.name, 'ram': device.ram, 'disk_size': device.disk_size, 'cpus': device.cpus, 'cell_id': device.cell_id, 'disk_image': device.disk_image, 'token': device.token, 'console_port': device.console_port}
     form = DeviceForm(data)
     self.assertTrue(form.is_valid())
   
   def test_device_name_max_length(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'a'*101, '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     data = {'name': device.name, 'ram': device.ram, 'disk_size': device.disk_size, 'cpus': device.cpus, 'cell_id': device.cell_id, 'disk_image': device.disk_image, 'token': device.token, 'console_port': device.console_port}
     form = DeviceForm(data)
     self.assertFalse(form.is_valid())
     
   def test_device_ram_max_length(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '9'*9, 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     data = {'name': device.name, 'ram': device.ram, 'disk_size': device.disk_size, 'cpus': device.cpus, 'cell_id': device.cell_id, 'disk_image': device.disk_image, 'token': device.token, 'console_port': device.console_port}
     form = DeviceForm(data)
     self.assertFalse(form.is_valid())
 
   def test_device_token_max_length(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token'*8, 10015)
     data = {'name': device.name, 'ram': device.ram, 'disk_size': device.disk_size, 'cpus': device.cpus, 'cell_id': device.cell_id, 'disk_image': device.disk_image, 'token': device.token, 'console_port': device.console_port}
     form = DeviceForm(data)
     self.assertFalse(form.is_valid())
 
   def test_device_unique_name(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     device = create_device(self, 'test_device', '2048', 25, 2, 15, image, 'this-is-a-made-up-token', 10015)
     device.save()
     with self.assertRaises(Exception) as raised:
@@ -202,11 +203,11 @@ class XmlViewTest(TestCase):
 class ImageViewTest(TestCase):
 
   def test_image_retrieval(self):
-    image = create_image(self, 'test_image', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image', 'pc')
     image.save()
-    image2 = create_image(self, 'test_image_2', 'router', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image2 = create_image(self, 'test_image_2', 'router',)
     image2.save()
-    image3 = create_image(self, 'test_image_3', 'switch', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image3 = create_image(self, 'test_image_3', 'switch',)
     image3.save()
 
     image_dict = {'disk_images': json.loads(serialize('json', DiskImage.objects.all()))}
@@ -235,7 +236,7 @@ class ImageViewTest(TestCase):
 class DeviceViewTest(TransactionTestCase):
 
   def test_device_creation(self):
-    image = create_image(self, 'test_image_4', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image_4', 'pc')
     image.save()
     image = DiskImage.objects.get(name='test_image_4')
     url = reverse('post_device_form')
@@ -259,7 +260,7 @@ class DeviceViewTest(TransactionTestCase):
     )
   
   def test_device_creation_wrong_request(self):
-    image = create_image(self, 'test_image_4', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image_4', 'pc')
     image.save()
     url = reverse('post_device_form')
     resp = self.client.post(
@@ -284,7 +285,7 @@ class DeviceViewTest(TransactionTestCase):
     )
   
   def test_device_creation_invalid_data(self):
-    image = create_image(self, 'test_image_4', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image_4', 'pc')
     image.save()
     url = reverse('post_device_form')
     resp = self.client.post(
@@ -309,7 +310,7 @@ class DeviceViewTest(TransactionTestCase):
     )
 
   def test_device_name_with_space(self):
-    image = create_image(self, 'test_image_4', 'pc', SimpleUploadedFile('../ubuntu-18.04.2-live-server-amd64.iso'))
+    image = create_image(self, 'test_image_4', 'pc')
     image.save()
     url = reverse('post_device_form')
     resp = self.client.post(
