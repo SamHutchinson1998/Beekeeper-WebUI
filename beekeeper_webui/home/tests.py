ab24b4aefdd3fdfff02a1899b4138a90b1ec7742
SamHutchinson1998 <sam.hutchinson1998@btinternet.com>
2020-04-26 13:08:39 +0100

    commit

--- beekeeper_webui/home/tests.py
+++ beekeeper_webui/home/tests.py
@@ -19,7 +19,7 @@ import os
 # Models tests
 
 def create_image(self, name, devicetype):
-  file_path = os.path.join(settings.MEDIA_ROOT, 'disk_images/TempleOS_1.ISO')
+  file_path = os.path.join(settings.MEDIA_ROOT, 'disk_images/TempleOS.ISO')
   diskimage = open(file_path, 'rb')
   return DiskImage.objects.create(name=name, devicetype=devicetype, disk_image=SimpleUploadedFile(diskimage.name, diskimage.read(), content_type='multipart/form-data'))
 
